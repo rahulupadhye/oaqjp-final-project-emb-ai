1d5482a6c255f2b3b5bb62c61a6cb3ef52eda469
rahulupadhye <rahulupadhye@yahoo.com>
2025-03-27 01:27:47 -0400

    After Task 3

--- emotion_detection.py
+++ emotion_detection.py
@@ -2,6 +2,7 @@
 This module provide a simplified wrapper over the 
 IBM Watson NLP library Emotion Predict
 '''
+import json
 import requests
 
 # Define function to run emotion detection using
@@ -22,5 +23,44 @@ def emotion_detector(text_to_analyze):
 
     # Sending a POST request to the emotion analysis API
     response = requests.post(url, json=myobj, headers=header)
+    formatted_res = json.loads(response.text)
 
-    return response.text
+    if response.status_code == 200:
+        try:
+            emotions = formatted_res['emotionPredictions'][0]['emotion']
+            anger_score = emotions['anger']
+            disgust_score = emotions['disgust']
+            fear_score = emotions['fear']
+            joy_score = emotions['joy']
+            sadness_score = emotions['sadness']
+            dominant_emotion = max(emotions, key=emotions.get) if any(emotions.values()) else None
+
+            emotion_scores = {
+                'anger': anger_score,
+                'disgust': disgust_score,
+                'fear': fear_score,
+                'joy': joy_score,
+                'sadness': sadness_score,
+                'dominant_emotion' : dominant_emotion
+            }
+
+            return emotion_scores
+        except (KeyError, IndexError, TypeError):
+            return {
+                'anger': None,
+                'disgust': None,
+                'fear': None,
+                'joy': None,
+                'sadness': None,
+                'dominant_emotion': None
+            }
+
+    return {
+        'anger': None,
+        'disgust': None,
+        'fear': None,
+        'joy': None,
+        'sadness': None,
+        'dominant_emotion': None
+    }
+
