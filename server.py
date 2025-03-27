1e596c04cf2d77b260815ab4a093bf041d2e5b11
rahulupadhye <rahulupadhye@yahoo.com>
2025-03-27 02:39:52 -0400

    After Task 6

---
+++ server.py
@@ -0,0 +1,32 @@
+'''
+Emotion Detection Server configuration module
+'''
+# imports
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
+
+# Define the Emotion Detector application
+app = Flask("Emotion Detector")
+
+# Route for emotion detector service
+@app.route("/emotionDetector")
+def emotion_detetion_action():
+    """Emotion detection handler."""
+    text_to_analyze = request.args.get('textToAnalyze')
+
+    response = emotion_detector(text_to_analyze)
+    emotions = ''.join([f"'{key}': {value}, " for key, value in response.items()])
+    text = f'''
+        For the given statement, the system response
+        is {emotions}. The dominant emotion 
+        is <b>{response['dominant_emotion']}</b>.
+    '''
+    return text
+
+@app.route("/")
+def render_index_page():
+    """Index page handler"""
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
